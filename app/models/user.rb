95e8f16b30be745940de4ead09fcc381c6462dd8
alexk6 <ukira1@gmail.com>
2012-12-20 22:30:43 +0900

    머지함

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,10 +17,17 @@ class User < ActiveRecord::Base
   has_many :microposts, dependent: :destroy
   #relationship과 1:n의 관계에서 나를 가리키게 할 key를 정할때, foreign_key를 쓴다.
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  #본 객체의 followed_users라는 변수를 선언하면서, relationship이라는 객체를 이용할 것이다.
+  #따라서 relationships에서 follower_id가 현재 객체인 녀석들을 잡아서, 해당 row의 followed 컬럼에 있는 녀석들을 followed_user로 받아낸다.
+  #relationship클래스 내에 followed가 user클래스로 되어있기 때문에 객체 타입은 문제가 없다.
   has_many :followed_users, through: :relationships, source: :followed
+  #위의 relationship의 반대버전이다. follower_id대신에 followed_id를 키로 사용하고 있고,
+  #reverse_relationship으로 클래스를 찾을수 없기 때문에 class_name으로 클래스명을 찾아주었다.
   has_many :reverse_relationships, foreign_key: "followed_id",
                                    class_name:  "Relationship",
                                    dependent:   :destroy
+  #본클래스를 follow하는 사용자들이다. 
+  #reverse_relationships를 through로 설정하였고, 해당 클래스의 follower를 잡아서 has_many로 넣어준다.
   has_many :followers, through: :reverse_relationships, source: :follower
   before_save { |user| user.email = email.downcase }
   before_save :create_remember_token
